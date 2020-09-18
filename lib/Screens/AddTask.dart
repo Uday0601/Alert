be7d38bf98508d0440167ac04ab0ad44ac331a92
Uday Krishna <nudaykrishna0601@gmail.com>
2020-09-18 10:19:44 +0530

    main project

---
+++ lib/Screens/AddTask.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddTask extends StatelessWidget {
+  static String routeName = 'AddTask';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('AddTask'),
+      ),
+    );
+  }
+}
